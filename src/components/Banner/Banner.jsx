04703c36d6075edfc32a603da3c55f802092c48a
momin755 <mominislamshamim@gmail.com>
2025-03-30 07:45:54 +0100

    Solved

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div className='container mx-auto my-5'>
-            <div className="bg-[url(../../../public/assets/hero.png)] bg-no-repeat bg-cover bg-center rounded-2xl py-40 space-y-8">
+            <div className="bg-[url(https://i.ibb.co.com/fGCypZzN/hero.png)] bg-no-repeat bg-cover bg-center rounded-2xl py-40 space-y-8">
             <h1 className='text-white text-5xl font-bold text-center leading-16'>Discover an exceptional cooking <br /> class tailored for you!</h1>
             <p className='text-xl text-white text-center'>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding <br /> problems to become an exceptionally well world-class Programmer.</p>
             <div className='flex gap-6 justify-center'>
